cd53f6935f726c18d9b7f9ff6666fa71d9b78768
Aleksandrs Zizkuns <sasha_zhizhkun@yahoo.com>
2018-08-23 15:30:57 +0000

    Leaderboards should update now

--- Assets/Scripts/Managers/GameMode/ObjectiveTracker.cs
+++ Assets/Scripts/Managers/GameMode/ObjectiveTracker.cs
@@ -13,7 +13,20 @@ public abstract class ObjectiveTracker : MonoBehaviour {
         {
             yield return null;
         }
+        if (GameModeManager.mode == GameMode.LimitedTurns)
+        {
+            Social.ReportScore(SafeMemory.GetInt("score"), "CgkI-MWprNwaEAIQAg", (bool success) =>
+            {
 
+            });
+        }
+        else if (GameModeManager.mode == GameMode.TimeAttack)
+        {
+            Social.ReportScore(SafeMemory.GetInt("score"), "CgkI-MWprNwaEAIQAw", (bool success) =>
+            {
+
+            });
+        }
         MenuOpener menuOpener = transform.GetComponentInParent<MenuOpener>();
         menuOpener.EndGame();
         yield return StartCoroutine(menuOpener.ToggleMenu());
--- Assets/Scripts/Managers/PlayServices/Leaderboards.cs
+++ Assets/Scripts/Managers/PlayServices/Leaderboards.cs
@@ -1,5 +1,6 @@
 ﻿//enum imitation for strings. play services use strings for leaderboard identification
 public static class Leaderboards
 {
-    public static string SomeLeaderboard = "LEADERBOARD_ID";
+    public static string TimeAttackLeaderboard = "CgkI-MWprNwaEAIQAw";
+    public static string LimitedTurnsLeaderboard = "CgkI-MWprNwaEAIQAg";
 }
